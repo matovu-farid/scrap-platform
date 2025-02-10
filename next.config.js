9d10c60bfb576cb5043ba4ab7227f97fc34f1e0b
matovu-farid <matovu90@gmail.com>
2025-02-10 17:50:53 +0300

    Update Next.js MDX configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
-const withMDX = require("@next/mdx")()
+// const withMDX = require("@next/mdx")()
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = withMDX(nextConfig)
+// import createMDX from '@next/mdx'
+import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   reactStrictMode: true,
   swcMinify: true,
-}
+  // Optionally, add any other Next.js config below
+};
 
-module.exports = withMDX(nextConfig)
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
