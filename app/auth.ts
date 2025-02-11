9d14196e9f18dcf6aa2f7bd3bbf4564f88ee9243
matovu-farid <matovu90@gmail.com>
2025-02-12 01:21:16 +0300

    Refactor NextAuth configuration to separate providers and experimental settings

--- app/auth.ts
+++ app/auth.ts
@@ -3,11 +3,19 @@ import NextAuth from "next-auth";
 import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "../auth.config";
-
+import Passkey from "next-auth/providers/passkey";
+import Resend from "next-auth/providers/resend";
 const prisma = new PrismaClient();
+
+const providers = {
+  ...authConfig.providers,
+  Passkey,
+  Resend,
+};
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  experimental: { enableWebAuthn: true },
   session: { strategy: "jwt" },
-  ...authConfig,
+  providers,
 });
