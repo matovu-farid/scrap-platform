7ac3c25584cfa1516b732b6de09b7bb4f017b19b
matovu-farid <matovu90@gmail.com>
2025-02-16 20:10:05 +0300

    Integrate React Query provider in root layout and simplify theme configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
-import "./globals.css"
-import { Inter } from "next/font/google"
-import { Toaster } from "@components/ui/toaster"
-import type React from "react"
-import { ThemeProvider } from "@components/theme-provider"
+import "./globals.css";
+import { Inter } from "next/font/google";
+import type React from "react";
+import { QueryClient } from "@tanstack/react-query";
+import Provider from "./provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "scrap-ai",
   description: "AI-powered web scraping made easy",
-}
+};
+
+// Create a client
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
-          <Toaster />
-        </ThemeProvider>
+        <Provider>{children}</Provider>
       </body>
     </html>
-  )
+  );
 }
-
---
+++ app/provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Toaster } from "@components/ui/toaster";
+import type React from "react";
+import { ThemeProvider } from "@components/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
